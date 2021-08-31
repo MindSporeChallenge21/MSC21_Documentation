c5f35a0ff4b82deabba83b40aa8a05686487579e
Eugene <lowzhao@gmail.com>
2021-08-31 15:10:27 +0800

    yes its working now

--- manual_deploy.sh
+++ manual_deploy.sh
@@ -4,29 +4,48 @@ set -e
 # install the plugins and build the static site
 gitbook install && gitbook build
 
+
+
+echo " ==== CHECKING OUT TO GH-PAGES ==== "
 # checkout to the gh-pages branch
 git checkout gh-pages
 
+
+echo " ==== REBASING GH-PAGES ==== "
 # pull the latest updates
 git pull origin gh-pages --rebase
 
+
+echo " ==== REPLACING FILES AT ROOT ==== "
 # copy the static site files into the current directory.
 cp -R _book/* .
 
+echo " ==== THROWING AWAY NODE MODULES ==== "
 # remove 'node_modules' and '_book' directory
 git clean -fx node_modules
+
+
+echo " ==== THROWING AWAY _BOOK FOLDER ==== "
 git clean -fx _book
 
+echo " ==== TELL GITHUB PAGES THIS IS NOT A JEKYLL PROJECT SO IT WILL RESPECT FILES THAT START WITH DOT ==== "
 touch .nojekyll
 
+echo " ==== ADDING FILES TO COMMIT ==== "
 # add all files
 git add .
 
+
+echo " ==== MAKING A COMMIT ==== "
 # commit
 git commit -a -m "Update docs"
 
+
+echo " ==== PUSHING THE COMMIT ==== "
 # push to the origin
 git push origin gh-pages
 
+
+echo " ==== COMING BACK TO MASTER ==== "
 # checkout to the master branch
 git checkout master
